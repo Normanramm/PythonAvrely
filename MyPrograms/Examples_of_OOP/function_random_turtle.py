f506b97bb3ef3fc527384a42de0f9fc51b79e4da
PythonAvrely <tesei87arieds@gmail.com>
2025-06-03 13:50:12 +0300

    # function random

--- MyPrograms/Examples_of_OOP/function_random_turtle.py
+++ MyPrograms/Examples_of_OOP/function_random_turtle.py
@@ -32,3 +32,36 @@ for _ in range(3):
     draw_random_polygon(6)
 
 turtle.done()
+
+
+# с добвалением as
+
+from random import uniform as random_uniform
+import turtle
+
+
+def random_color():
+    return random_uniform(0, 1), random_uniform(0, 1), (random_uniform(0, 1))
+
+
+def draw_random_polygon(sides=6):
+    t = turtle.Turtle()
+    t.speed(0)
+    t.pensize(random_uniform(1, 5))
+
+    for _ in range(sides):
+        t.color(random_color(), random_color())
+        t.begin_fill()
+        t.forward(random_uniform(50, 150))
+        t.left(360/sides)
+        t.end_fill()
+
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title("Случайные многоугольники")
+
+for _ in range(3):
+    draw_random_polygon(6)
+
+turtle.done()
