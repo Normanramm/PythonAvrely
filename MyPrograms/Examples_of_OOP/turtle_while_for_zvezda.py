7a1115ea80e096464a503f840c42013b2fc28345
PythonAvrely <tesei87arieds@gmail.com>
2025-06-05 19:58:02 +0300

    # while/for turtle zvezda

--- MyPrograms/Examples_of_OOP/turtle_while_for_zvezda.py
+++ MyPrograms/Examples_of_OOP/turtle_while_for_zvezda.py
@@ -6,6 +6,7 @@ t.speed(0)
 
 
 def zvezda():
+
     i = 0
     while i < 50:
         t.forward(200)
@@ -15,6 +16,7 @@ def zvezda():
 
 
 def zvezda_2():
+
     for steps in range(50):
         for c in ('blue', 'red', 'green', 'yellow', "#EE07C4"):
             t.width(3)
@@ -24,12 +26,13 @@ def zvezda_2():
 
 
 colors = ['red', 'orange', 'yellow', 'green',
-          'blue', 'purple', 'pink', 'violet', "#36B186"]
+          'blue', 'purple', 'pink', 'violet']
 
 
 def zvezda_3():
     i = 0
     while i < 50:
+        # Выбираем случайный цвет для каждого луча
         t.color(random.choice(colors))
         t.forward(130)
         t.backward(70)
@@ -58,5 +61,53 @@ zvezda_2()
 two_line()
 zvezda_3()
 
+
 turtle.done()
 
+
+# _____________________________________________________________
+
+
+t = turtle.Turtle()
+t.speed(0)
+
+while True:
+    t.forward(200)
+    t.left(170)
+    if abs(t.pos()) < 1:
+        break
+
+
+turtle.done()
+
+# ________________________________________________________________
+
+
+t = turtle.Turtle()
+t.speed(0)
+
+while True:
+    t.forward(200)
+    t.left(170)
+    t.backward(10)
+    if t.distance(0, 0) < 3:
+        break
+
+
+turtle.done()
+
+
+# _____________________________________________________________
+
+
+t = turtle.Turtle()
+t.speed(0)
+
+i = 0
+while i < 50:
+    t.forward(200)
+    t.left(170)
+    t.backward(10)
+    i += 1
+
+turtle.done()
---
+++ MyPrograms/main.py
@@ -0,0 +1,10 @@
+text = input("Введите строку: ")
+# i = 0
+
+# while i < len(text):
+#     print(text[i])
+#     i += 1
+
+
+for i in (text):
+    print(i)
