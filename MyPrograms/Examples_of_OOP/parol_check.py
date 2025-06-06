60f85101ac1e104f2511400d182b73d5dc7e460a
PythonAvrely <tesei87arieds@gmail.com>
2025-06-06 18:18:43 +0300

    # oop zapros parol

--- MyPrograms/Examples_of_OOP/parol_check.py
+++ MyPrograms/Examples_of_OOP/parol_check.py
@@ -46,3 +46,30 @@ class PasswordChecker:
 if __name__ == "__main__":
     checker = PasswordChecker()
     checker.process_access()
+
+
+# _________________________________________________________________
+# изменил и добавил break
+
+class PasswordChecker:
+    def __init__(self, correct_password='123456'):
+        self.correct_password = correct_password
+
+    def check_password(self):
+        for i in range(3):
+            user_password = input("Введите пароль: ")
+            if user_password == self.correct_password:
+                break
+        else:
+            print("Доступ запрещен")
+            return False
+        return True
+
+    def process_access(self):
+        if self.check_password():
+            print("Доступ разрешен")
+
+
+if __name__ == "__main__":
+    checker = PasswordChecker()
+    checker.process_access()
--- MyPrograms/main.py
+++ MyPrograms/main.py
@@ -3,20 +3,20 @@ class PasswordChecker:
         self.correct_password = correct_password
 
     def check_password(self):
-        user_password = input("Введите пароль: ")
-        if user_password == self.correct_password:
-            return True
+        for i in range(3):
+            user_password = input("Введите пароль: ")
+            if user_password == self.correct_password:
+                break
         else:
             print("Доступ запрещен")
             return False
+        return True
 
     def process_access(self):
         if self.check_password():
             print("Доступ разрешен")
 
 
-
 if __name__ == "__main__":
     checker = PasswordChecker()
     checker.process_access()
-
