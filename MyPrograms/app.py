83a917e9345ed895c39576aebc3970f5dad06443
PythonAvrely <tesei87arieds@gmail.com>
2025-06-27 18:31:39 +1000

    # попытка в flask

--- MyPrograms/app.py
+++ MyPrograms/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Привет, Flask!'
+# Класс ToDoList
+class ToDoList:
+    def __init__(self):
+        self.tasks = []
 
-@app.route('/user/<username>')
-def profile(username):
-    return f'Профиль пользователя {username}'
+    def add_task(self, task):
+        if task.strip() != "":
+            self.tasks.append(task)
+
+    def get_tasks(self):
+        return self.tasks
+
+# Создаем экземпляр списка задач
+todo_list = ToDoList()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task = request.form.get('task')
+        todo_list.add_task(task)
+        return redirect(url_for('index'))
+
+    tasks = todo_list.get_tasks()
+    return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
