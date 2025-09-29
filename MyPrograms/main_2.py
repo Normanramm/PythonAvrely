a903a07d8cafd4b8d690ac86cd457b3b6b956706
PythonAvrely <tesei87arieds@gmail.com>
2025-09-29 19:12:06 +1000

    # TicTacToe

--- MyPrograms/main_2.py
+++ MyPrograms/main_2.py
@@ -8,13 +8,28 @@ class TicTacToe:
         self.root.title("Крестики нолики")
         # self.root.geometry("400x400")  # Отключаем фиксированный размер
         self.root.iconbitmap("icon1.ico")  # создает иконку
-
+        self.current_player = "X"  # Обработка хода, первым ходит "X" (крестик)
         self.buttons = []  # Сюда будем сохранять все кнопки
 
         # Предназначен для внутреннего использования при инициализации. Принцип инкапсуляции: объект должен быть готов к работе сразу после создания.
         # Пользователь класса (или ты сам завтра) не должен помнить, что нужно дополнительно вызывать create_widgets().
         # Так объект будет готов к работе сразу после создания.
         self.create_widgets()
+
+    def on_click(self, index):
+        """Функция обработки клика"""
+        # global current_player  # Используем глобальную переменную(Удалено — используется self.current_player)!!!
+
+        # Проверяем, пустая ли кнопка
+        if self.buttons[index]["text"] == "":
+            # Записываем X или O
+            self.buttons[index]["text"] = self.current_player
+
+            # Меняем игрока
+            if self.current_player == "X":
+                self.current_player = "O"
+            else:
+                self.current_player = "X"
 
     def create_widgets(self):
         """Создаёт игровое поле из 9 кнопок."""
@@ -24,10 +39,13 @@ class TicTacToe:
                 text="",  # Пока без текста
                 font=("Arial", 30),  # Крупный шрифт
                 width=5,  # Ширина кнопки
-                height=2  # Высота кнопки
+                height=2,  # Высота кнопки
+                # При клике вызываем метод on_click с нужным индексом
+                command=lambda idx=i: self.on_click(idx)
             )
             # Размещаем кнопку в сетке(номер строки, номер столбца)
-            button.grid(row=i//3, column=i % 3)  # Размещаем кнопку в сетке, тут можно добавить другие параметры
+            # Размещаем кнопку в сетке, тут можно добавить другие параметры
+            button.grid(row=i//3, column=i % 3)
             self.buttons.append(button)  # Добавляем кнопку в список
 
     def run(self):
