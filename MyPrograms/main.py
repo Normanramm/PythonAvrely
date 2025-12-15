11a7a606bb475d39474f9f93bc66eda8625f9711
PythonAvrely <tesei87arieds@gmail.com>
2025-12-15 20:24:15 +1000

    # просто задача

--- MyPrograms/main.py
+++ MyPrograms/main.py
@@ -1,24 +1,14 @@
-def solution(s):
-    if not s:
-        return s  
-    
-    result = [s[0]]  
-    
-    for char in s[1:]:
-        if char.isupper():
-            result.append(' ')  
-        result.append(char)
-    
-    return ''.join(result)
-    
-print(solution("camelCase"))         
-print(solution("breakCamelCase"))    
-print(solution("HelloWorld"))        
-print(solution("A"))                 
-print(solution(""))                  
-print(solution("oneTwoThree"))       
+name = input("Привет! Как тебя зовут? ")
+print(f"Привет, {name}!")
+age_input = input("Сколько тебе лет ")
+age = int(age_input)
 
+bot_age = 2
+difference = age - bot_age
+print(f"Ты на {difference} лет старше меня. Мне всего {bot_age} года!")
 
-
-
-
+car = input("Какая марка машины у тебя любимая? ")
+if car == "toyota crown":
+    print(f"Ммм {car} — идеальный автомобиль")
+else:
+    print(f"Ммм {car} — отличная марка, но не такая шикарная, как Lada.")
