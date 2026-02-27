20a9412d2b2e9755b165ca0c790f0cd5eec1541a
PythonAvrely <tesei87arieds@gmail.com>
2026-02-27 21:05:03 +1000

    # 0.2.1

--- MyPrograms/Teacher/Kaidzen_JS_HTML_CSS/script/kaidzen.js
+++ MyPrograms/Teacher/Kaidzen_JS_HTML_CSS/script/kaidzen.js
@@ -1,2 +1,4 @@
 console.log("этот текст я ввел в kaidzen.js");
 alert("Текст выведен из kaidzen.js");
+elem.innerHTML = "Доступ через id делается через решетку #elem в СSS";
+document.getElementsByClassName("elem")[0].innerHTML = "Доступ через class делается через точку .elem в CSS";
