d57b2d14ae1667cd9657325acec618d6749a1fdf
PythonAvrely <tesei87arieds@gmail.com>
2026-04-19 00:38:26 +1000

    # работа с HTML

--- MyPrograms/main.py
+++ MyPrograms/main.py
@@ -1,3 +1,23 @@
 
+                            
+                            # Итератор
+my_list = [1, 2, 3]
+my_iter = iter(my_list)
+next(my_iter)  # 1
+next(my_iter)  # 2
+# Генератор
+def count_up_to(n):
+    count = 1
+    while count <= n:
+        yield count
+        count += 1
+for num in count_up_to(5):
+    print(num)
+# Generator expression
+squares = (x**2 for x in range(10))
+# itertools
+from itertools import count, cycle, chain
+for i in count(0, 2):
+    print(i)  # 0, 2, 4...
 
 
