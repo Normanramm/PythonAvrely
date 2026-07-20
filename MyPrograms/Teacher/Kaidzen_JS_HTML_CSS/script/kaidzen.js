b51e3102c16b202bc0cdc87938f5d5d3ec314e8b
PythonAvrely <tesei87arieds@gmail.com>
2026-07-20 21:25:41 +1000

    # решение задач и тестироние консоли js

--- MyPrograms/Teacher/Kaidzen_JS_HTML_CSS/script/kaidzen.js
+++ MyPrograms/Teacher/Kaidzen_JS_HTML_CSS/script/kaidzen.js
@@ -1,5 +1,15 @@
-console.log("этот текст я ввел в kaidzen.js");
-// alert("Текст выведен из kaidzen.js");
+// console.log("test1")
+// let a = 1
+// let b = 2
+// console.log("test2")
+// console.log(a + b)
+// console.log("test3")
 
+// const text = "Hello, 'World'"
+// console.log(text)
+
+console.log("этот текст я ввел в kaidzen.js");
+// // alert("Текст выведен из kaidzen.js");
 elem.innerHTML = "Доступ через id делается через решетку #elem в СSS";
 document.getElementsByClassName("elem")[0].innerHTML = "Доступ через class делается через точку .elem в CSS";
+
